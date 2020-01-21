119af85508906d175651c03c4f344db07a23ae41
top-notch07 <59329559+top-notch07@users.noreply.github.com>
2020-01-21 22:23:46 +0530

    Delete S_Program4.py

--- S_Program4.py
+++
@@ -1,9 +0,0 @@
-str = input("Enter any sentence\n")
-lowerCount = 0
-upperCount = 0
-for i in str:
-    if i>='a' and i <= 'z':
-        lowerCount += 1
-    elif i>='A' and i <= 'Z':
-        upperCount += 1
-print("LowerCase: ",lowerCount,"\nUpperCase: ",upperCount)
