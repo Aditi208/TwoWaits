e10d2ed8bd03dea481a044cb22746a114e3c5461
top-notch07 <59329559+top-notch07@users.noreply.github.com>
2020-01-21 11:02:26 +0530

    Delete Program2.py

--- Program2.py
+++
@@ -1,9 +0,0 @@
-n=5
-for i in range(1,6):
-    for j in range(1,(2*n-1)+1):
-        if(j>=(n-(i-1)) and j<=(n+(i-1))):
-           print("*",end="")
-        else:
-            print(" ",end="")
-    print("\n")
-           
